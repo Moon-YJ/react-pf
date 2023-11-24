361310040fd7bc5ae9ee4a0e69ccb0c872478d3e
Moon-YJ <tunjeong@gmail.com>
2023-11-24 12:53:11 +0900

    custom hook을 생성해서 컴포넌트에서 재활용하기

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,10 +1,17 @@
 import { useEffect, useRef } from 'react';
 import './Layout.scss';
+import { useSplitText } from '../../../hooks/useSplitText';
 
 export default function Layout({ children, title }) {
 	const refFrame = useRef(null);
+	// useEffect 내부에서 자주 쓰일만한 특정 함수를 호출해야할때
+	// use로 시작하는 커스텀hook은 특정함수 안쪽에서 호출 불가
+	// 따라서, 해당 hook이 리턴으로 함수를 반환하도록 처리
+	const splitTxt = useSplitText();
 
 	useEffect(() => {
+		// 아래처럼 custom hook이 반환한 함수를 hook이나 핸들러함수 내부에서 사용 가능
+		splitTxt('hello');
 		refFrame.current.classList.add('on');
 	}, []);
 
--- src/hooks/useSplitText.js
+++ src/hooks/useSplitText.js
@@ -1,3 +1,13 @@
+/*
+  - use로 시작하는 커스텀hook 함수는 컴포넌트단에서 호출 가능
+  - 컴포넌트 안쪽의 또다른 hook이나 일반 핸들러 함수 안쪽에서는 호출 불가능
+  - 해결방법: 커스텀hook이 특정 기능을 수행해주는 또 다른 함수를 내부적으로 생성한 다음, 해당 함수를 리턴
+  - 일반 핸들러 함수 안쪽에서 커스텀hook자체는 호출 불가하지만 커스텀hook이 리턴한 자식 함수는 호출 가능
+*/
+
 export function useSplitText(txt) {
-	console.log(txt);
+	// 해당 useSplitText hook은 아래의 함수를 리턴
+	return (txt) => {
+		console.log(txt);
+	};
 }
