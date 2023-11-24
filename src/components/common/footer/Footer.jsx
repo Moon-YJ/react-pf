ad5d2253ee19d4c3a891d360ce19f745a880e56c
Moon-YJ <tunjeong@gmail.com>
2023-11-24 11:19:48 +0900

    라우터, 헤더, 푸터 작업

---
+++ src/components/common/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import './Footer.scss';
+import { FaFacebookF, FaTwitter, FaYoutube } from 'react-icons/fa';
+// npm i reac
+
+export default function Footer() {
+	return (
+		<footer className='Footer'>
+			<h1>DCODELAB</h1>
+
+			<p>2023 DCODELAB &copy; All Rights Reserved.</p>
+
+			<ul>
+				<li>
+					<FaFacebookF />
+					{/* <FaInstagram color={'hotpink'} size={30} /> */}
+				</li>
+				<li>
+					<FaTwitter />
+				</li>
+				<li>
+					<FaYoutube />
+				</li>
+			</ul>
+		</footer>
+	);
+}
