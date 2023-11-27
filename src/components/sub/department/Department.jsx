b6d054b86af50ecbcbd22d00a47d31a06b167927
Moon-YJ <tunjeong@gmail.com>
2023-11-27 13:02:54 +0900

    Object.keys()와 Object.values() 활용하기

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -4,12 +4,18 @@ import './Department.scss';
 
 export default function Department() {
 	const [MemberData, setMemberData] = useState([]);
+	const [MemberTit, setMemberTit] = useState('');
+
 	const path = process.env.PUBLIC_URL; // public폴더까지의 경로를 구하는 구문
 	const fetchDepartment = () => {
 		fetch(`${path}/DB/department.json`)
 			.then((data) => data.json())
 			.catch((err) => console.log(err))
-			.then((json) => setMemberData(json.members));
+			.then((json) => {
+				//setMemberData(Object.values(json)[0]); // 객체 반복돌면서 value값만 배열로 반환
+				setMemberData(json.members);
+				setMemberTit(Object.keys(json)[0]); //객체 반복돌면서 key값만 배열로 반환
+			});
 	};
 	useEffect(() => {
 		fetchDepartment();
@@ -17,13 +23,20 @@ export default function Department() {
 
 	return (
 		<Layout title={'Department'}>
-			{MemberData.map((member, idx) => {
-				return (
-					<div key={member + idx}>
-						<h2>{member.name}</h2>
-					</div>
-				);
-			})}
+			<section className='memberBox'>
+				<h2>{MemberTit}</h2>
+				{MemberData.map((member, idx) => {
+					return (
+						<article key={member + idx}>
+							<div className='pic'>
+								<img src={`${path}/img/${member.pic}`} alt={member.name} />
+							</div>
+							<h2>{member.name}</h2>
+							<p>{member.position}</p>
+						</article>
+					);
+				})}
+			</section>
 		</Layout>
 	);
 }
@@ -34,4 +47,11 @@ export default function Department() {
 		2. fetch문을 이용해서 특정 URL의 데이터를 가져온 뒤,동기적으로 배열로 뽑음. 그 다음에 state에 담아주는 함수 정의
 		3. 위에서 만든 함수를 의존성 배열이 비어있는 useEffect문 안쪽에서 호출 (다음번 랜더링 타이밍에 state값 활용 가능)
 		4. state에 담겨있는 데이터 배열값을 map으로 반복 돌면서 JSX 구문 생성
+
+	- 객체의 property에서 key와 value값 반복 도는 방법
+		ex. const student = {name: 'David', age: 20}
+		- key 반복 돌면서 배열 반환
+		: Object.keys(student) //['name', 'age']
+		- value 반복 돌면서 배열 반환
+		: Object.values(student) //['David', 20]
 */
