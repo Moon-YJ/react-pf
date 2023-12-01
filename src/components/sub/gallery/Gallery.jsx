e8ff5b6cd5f817b59ec42aad259c8cfeb739733f
Moon-YJ <tunjeong@gmail.com>
2023-12-01 12:09:15 +0900

    profile 이미지 없을때 default이미지 onError로 등록

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -33,13 +33,14 @@ export default function Gallery() {
 								/>
 							</div>
 							<h2>{pic.title}</h2>
-							<div className='info'>
+							<div className='profile'>
 								<img
 									src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
 									alt={pic.owner}
+									onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
 								/>
+								<span>{pic.owner}</span>
 							</div>
-							<p>{pic.owner}</p>
 						</article>
 					);
 				})}
