3cbf9a861c1cb2e4420f63fb713ddb6e9cd54c0d
Moon-YJ <tunjeong@gmail.com>
2023-12-05 10:53:56 +0900

    검색 결과값 없는 경우 처리

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -72,6 +72,9 @@ export default function Gallery() {
 
 		const data = await fetch(url);
 		const json = await data.json();
+		/*
+			if (json.photos.photo.length === 0) return alert('해당 검색어의 결과값이 없습니다.');
+		*/
 		setPics(json.photos.photo);
 	};
 
@@ -100,27 +103,31 @@ export default function Gallery() {
 
 			<section>
 				<Masonry className={'container'} options={{ transitionDuration: '0.5s', gutter: 20 }}>
-					{Pics.map((pic, idx) => {
-						return (
-							<article key={pic.id}>
-								<div className='pic'>
-									<img
-										src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-										alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-									/>
-								</div>
-								<h2>{pic.title}</h2>
-								<div className='profile'>
-									<img
-										src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
-										alt={pic.owner}
-										onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
-									/>
-									<span onClick={handleOwner}>{pic.owner}</span>
-								</div>
-							</article>
-						);
-					})}
+					{Pics.length === 0 ? (
+						<h2>해당 검색어의 결과값이 없습니다.</h2>
+					) : (
+						Pics.map((pic, idx) => {
+							return (
+								<article key={pic.id}>
+									<div className='pic'>
+										<img
+											src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+											alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+										/>
+									</div>
+									<h2>{pic.title}</h2>
+									<div className='profile'>
+										<img
+											src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+											alt={pic.owner}
+											onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
+										/>
+										<span onClick={handleOwner}>{pic.owner}</span>
+									</div>
+								</article>
+							);
+						})
+					)}
 				</Masonry>
 			</section>
 		</Layout>
