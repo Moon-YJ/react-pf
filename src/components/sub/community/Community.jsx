c52e11b4a917afe9c115fcb9c5c17548e68971d0
Moon-YJ <tunjeong@gmail.com>
2023-12-06 12:38:45 +0900

    Local storage 관련 설명 주석

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -8,7 +8,6 @@ export default function Community() {
 	const [Post, setPost] = useState([]);
 	const refTit = useRef(null);
 	const refCon = useRef(null);
-	console.log(Post);
 
 	const resetPost = (e) => {
 		refTit.current.value = '';
@@ -16,17 +15,13 @@ export default function Community() {
 	};
 
 	const createPost = (e) => {
-		if (
-			!refTit.current.value.trim() ||
-			!refCon.current.value.trim() ||
-			refCon.current.value.length > 10 ||
-			refCon.current.value.length > 300
-		) {
+		if (!refTit.current.value.trim() || !refCon.current.value.trim()) {
 			resetPost();
 			return alert('제목과 본문을 모두 작성해 주세요');
 		}
-		if (refCon.current.value.length > 10 || refCon.current.value.length > 300) {
-			return alert('제목은 10글자, 본문은 300글자 미만으로 작성해 주세요');
+		console.log(refTit.current.value.length);
+		if (refTit.current.value.length > 5 || refCon.current.value.length > 300) {
+			return alert('제목은 5글자, 본문은 300글자 미만으로 작성해 주세요');
 		}
 		setPost([{ title: refTit.current.value, content: refCon.current.value }, ...Post]);
 	};
@@ -70,16 +65,26 @@ export default function Community() {
 }
 
 /*
-
 	Create (데이터 저장) - 글 작성
 	Read (데이터 호출) - 글 목록보기
 	Update (데이터 변경) - 글 수정
 	Delete(데이터 삭제) - 글 삭제
 
 	1. 글 입력 박스, 글 출력박스 생성
-	2. 전체글을 관리할 배열 state를 생성 [{글정보1},{글정보2},{글정보3}]
+	2. 전체글을 관리할 배열 state를 생성 [{글정보1}, {글정보2}, {글정보3}]
 	3. 글 입력박스에 글 입력 후 저장 버튼 클릭시 글 정보를 객체형태로 state 계속 추가 (Create)
 	4. 배열 state에 추가된 값들을 반복돌면서 글 리스트 출력 (Read)
 	5. 글 출력시 삭제버튼, 수정버튼 추가해서 출력
 	6. 글 리스트에서 삭제버튼 클릭시 해당 배열 state에서 이벤트가 발생한 순번의 객체를 제거해서 글 삭제 (Delete)
+*/
+
+/*
+	LocalStorage: 모든 브라우저가 내장하고있는 경량의 저장소(콘솔창 Application에서 확인가능)
+	- 문자값만 저장 가능 (5MB)
+	- 로컬저장소에 문자열 이외의 값을 저장할때는 강제로 문자화시켜서 저장
+	- 로컬저장소의 값을 JS로 가져올때는 문자값을 반대로 객체화시켜서 호출
+	
+	LocalStorage객체에 활용가능한 메서드
+	- setItem('key', '문자화된 데이터'); //해당 키값에 데이터를 담아서 저장
+	- getItem('key'); //해당 키값에 매칭되는 데이터를 가져옴
 */
