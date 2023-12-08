def256d339a2aa4f7e84023da1bb46473f141c39
Moon-YJ <tunjeong@gmail.com>
2023-12-08 13:04:48 +0900

    마커 출력 및 원하는 이미지로 변경

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,18 +1,34 @@
 import { useEffect, useRef } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
-//9ccbf4d9fc75facc6101ad5e5cad392e (js key값)
 
 export default function Contact() {
-	const mapFrame = useRef(null);
 	const { kakao } = window;
+	const mapFrame = useRef(null);
+
 	const mapOption = useRef({
-		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-		level: 3, // 지도의 확대 레벨
+		// 위치값 정밀하게 보정하는 법
+		// 기존 구글지도 위치값 복사한 뒤, 카카오예제의 클릭한위치의 마커표시 직접해보기에서
+		// 해당 코드를 붙여넣기 하고 원하는 지점을 찍으면 아래와 같이 정밀한 수치값을 확인 가능
+		center: new kakao.maps.LatLng(37.51264336059771, 127.05882782975769),
+		level: 3,
 	});
 
+	const imgSrc = process.env.PUBLIC_URL + '/img/marker1.png';
+	const imgSize = new kakao.maps.Size(232, 99);
+	// 마커 이미지의 꼭지점 이동 (이미지 넓이의 절반만큼, 높이만큼)
+	const imgOption = { offset: new kakao.maps.Point(116, 99) };
+
 	useEffect(() => {
-		var map = new kakao.maps.Map(mapFrame.current, mapOption.current);
+		const mapInstance = new kakao.maps.Map(mapFrame.current, mapOption.current);
+		const markerImageInstance = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOption);
+
+		const markerInstance = new kakao.maps.Marker({
+			position: mapOption.current.center,
+			image: markerImageInstance,
+		});
+
+		markerInstance.setMap(mapInstance);
 	}, []);
 
 	return (
