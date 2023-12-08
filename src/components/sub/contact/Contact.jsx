d7af2735a48968e8e9beebf15b4ab036107c0f23
Moon-YJ <tunjeong@gmail.com>
2023-12-08 11:38:15 +0900

    kakao 지도 생성

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useRef } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
+//9ccbf4d9fc75facc6101ad5e5cad392e (js key값)
 
 export default function Contact() {
-	return <Layout title={'Contact'}>Contact</Layout>;
+	const mapFrame = useRef(null);
+	const { kakao } = window;
+	const mapOption = useRef({
+		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		level: 3, // 지도의 확대 레벨
+	});
+
+	useEffect(() => {
+		var map = new kakao.maps.Map(mapFrame.current, mapOption.current);
+	}, []);
+
+	return (
+		<Layout title={'Contact'}>
+			<article id='map' ref={mapFrame}></article>
+		</Layout>
+	);
 }
