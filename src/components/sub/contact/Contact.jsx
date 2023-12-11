3fbe82961c7e49195fc561c1cb0bfdb3ff3c9ef5
Moon-YJ <tunjeong@gmail.com>
2023-12-11 10:08:45 +0900

    kakao 참조객체에 담기

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -3,7 +3,7 @@ import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 
 export default function Contact() {
-	const { kakao } = window;
+	const kakao = useRef(window.kakao);
 
 	const [Index, setIndex] = useState(0);
 	const mapFrame = useRef(null);
@@ -16,33 +16,33 @@ export default function Contact() {
 			// 기존 구글지도 위치값 복사한 뒤, 카카오예제의 클릭한위치의 마커표시 직접해보기에서
 			// 해당 코드를 붙여넣기 하고 원하는 지점을 찍으면 아래와 같이 정밀한 수치값을 확인 가능
 			title: '삼성역 코엑스',
-			latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
+			latlng: new kakao.current.maps.LatLng(37.51100661425726, 127.06162026853143),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
 			// 마커 이미지 크기 지정
-			imgSize: new kakao.maps.Size(232, 99),
+			imgSize: new kakao.current.maps.Size(232, 99),
 			// 마커 이미지의 꼭지점 이동 (이미지 넓이의 절반만큼, 높이만큼)
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
 		},
 		{
 			title: '넥슨 본사',
-			latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+			latlng: new kakao.current.maps.LatLng(37.40211707077346, 127.10344953763003),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
 		},
 		{
 			title: '서울 시청',
-			latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+			latlng: new kakao.current.maps.LatLng(37.5662952, 126.9779451),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
 		},
 	]);
 
 	// 마커 인스턴스 생성 (브라우저 DOM의 기능이 필요없으므로 useEfeect 바깥에서 만들어도 됨)
-	markerInstance.current = new kakao.maps.Marker({
+	markerInstance.current = new kakao.current.maps.Marker({
 		position: mapInfo.current[Index].latlng,
-		image: new kakao.maps.MarkerImage(
+		image: new kakao.current.maps.MarkerImage(
 			mapInfo.current[Index].imgSrc,
 			mapInfo.current[Index].imgSize,
 			mapInfo.current[Index].imgPos
@@ -51,12 +51,12 @@ export default function Contact() {
 
 	// 컴포넌트 마운시 참조객체에 담아놓은 DOM 프레임에 지도 인스턴스 출력 및 마커 세팅
 	useEffect(() => {
-		const mapInstance = new kakao.maps.Map(mapFrame.current, {
+		const mapInstance = new kakao.current.maps.Map(mapFrame.current, {
 			center: mapInfo.current[Index].latlng,
 			level: 3,
 		});
 		markerInstance.current.setMap(mapInstance);
-	}, [Index, kakao]);
+	}, [Index]);
 
 	return (
 		<Layout title={'Contact'}>
