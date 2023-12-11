33df943739e01f6d3ebd1eaab025285e321151bb
Moon-YJ <tunjeong@gmail.com>
2023-12-11 09:54:27 +0900

    버튼 활성화는 동작하는데 지도는 안바뀌는 오류

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -3,23 +3,25 @@ import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 
 export default function Contact() {
-	const [Index, setIndex] = useState(1);
+	const [Index, setIndex] = useState(0);
 
 	const { kakao } = window;
 	const mapFrame = useRef(null);
+	const markerInstance = useRef(null);
+
 	// 지점마다 출력할 정보를 개별적인 객체로 묶어서 배열로 그룹화
 	const mapInfo = useRef([
 		{
-			title: '코엑스',
 			// 위치값 정밀하게 보정하는 법
 			// 기존 구글지도 위치값 복사한 뒤, 카카오예제의 클릭한위치의 마커표시 직접해보기에서
 			// 해당 코드를 붙여넣기 하고 원하는 지점을 찍으면 아래와 같이 정밀한 수치값을 확인 가능
-			latlng: new kakao.maps.LatLng(37.51264336059771, 127.05882782975769),
-			imgSrc: process.env.PUBLIC_URL + '/img/marker1.png',
+			title: '삼성역 코엑스',
+			latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
 			// 마커 이미지 크기 지정
 			imgSize: new kakao.maps.Size(232, 99),
 			// 마커 이미지의 꼭지점 이동 (이미지 넓이의 절반만큼, 높이만큼)
-			imgOption: { offset: new kakao.maps.Point(116, 99) },
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
 		},
 		{
 			title: '넥슨 본사',
@@ -37,23 +39,34 @@ export default function Contact() {
 		},
 	]);
 
-	// 마커 인스턴스 생성
-	const markerInstance = new kakao.maps.Marker({
+	// 마커 인스턴스 생성 (브라우저 DOM의 기능이 필요없으므로 useEfeect 바깥에서 만들어도 됨)
+	markerInstance.current = new kakao.maps.Marker({
 		position: mapInfo.current[Index].latlng,
 		image: new kakao.maps.MarkerImage(
 			mapInfo.current[Index].imgSrc,
 			mapInfo.current[Index].imgSize,
-			mapInfo.current[Index].imgOption
+			mapInfo.current[Index].imgPos
 		),
 	});
 
+	// 컴포넌트 마운시 참조객체에 담아놓은 DOM 프레임에 지도 인스턴스 출력 및 마커 세팅
 	useEffect(() => {
-		const mapInstance = new kakao.maps.Map(mapFrame.current, { center: mapInfo.current[Index].latlng, level: 3 });
-		markerInstance.setMap(mapInstance);
+		const mapInstance = new kakao.maps.Map(mapFrame.current, {
+			center: mapInfo.current[Index].latlng,
+			level: 3,
+		});
+		markerInstance.current.setMap(mapInstance);
 	}, []);
 
 	return (
 		<Layout title={'Contact'}>
+			<ul className='branch'>
+				{mapInfo.current.map((el, idx) => (
+					<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>
+						{el.title}
+					</button>
+				))}
+			</ul>
 			<article id='map' ref={mapFrame}></article>
 		</Layout>
 	);
