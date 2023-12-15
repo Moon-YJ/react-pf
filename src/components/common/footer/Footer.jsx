6fec756fec2c18abf401ceef5520eb6870d72c95
Moon-YJ <tunjeong@gmail.com>
2023-12-15 09:39:26 +0900

    주석 추가

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -11,6 +11,7 @@ export default function Footer() {
 			<h1>DCODELAB</h1>
 			{/* 아래 코드에서 조건문을 쓰는 이유 */}
 			{/* 첫번째 랜더링시에는 store부터 빈 배열이 전달되므로 두번째 랜더링부터 해당 구문이 실행되도록 조건문 처리 */}
+			{/* App.js 기준으로 Department 컴포넌트는 메인에서 값이 이미 담긴후에 접속하게돼서 데이터가 있는데 Footer.jsx는 바로 불러와지므로 해당 조건문 필요 */}
 			<p>{MemberData && `${MemberData[0].position} : ${MemberData[0].name}`}</p>
 
 			<ul>
