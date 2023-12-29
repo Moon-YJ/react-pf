a3db161ed981fd2eea9d7395369b5075e28b8f5a
Moon-YJ <tunjeong@gmail.com>
2023-12-29 15:01:17 +0900

    화살표 delay 호버 모션 작업(기본ver)

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -3,9 +3,11 @@ import './Pics.scss';
 export default function Pics() {
 	return (
 		<section className='Pics'>
-			<div className='box'></div>
-			<div className='box'></div>
-			<div className='box'></div>
+			<div className='box box1'>
+				<span></span>
+			</div>
+			<div className='box box2'></div>
+			<div className='box box3'></div>
 		</section>
 	);
 }
