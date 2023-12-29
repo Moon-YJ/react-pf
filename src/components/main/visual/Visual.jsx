bb220ca810070e74ccffa163c14c4835a3fdf2cf
Moon-YJ <tunjeong@gmail.com>
2023-12-29 10:12:29 +0900

    Swiper 설치 및 기본 구조 작업

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
 import './Visual.scss';
+// npm i swiper@8
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 export default function Visual() {
-	return <figure className='Visual'></figure>;
+	const { youtube } = useSelector(store => store.youtubeReducer);
+	return (
+		<figure className='Visual'>
+			<Swiper>
+				{youtube.map((vid, idx) => {
+					if (idx >= 5) return null;
+					return (
+						<SwiperSlide key={vid.id}>
+							<div className='inner'>
+								<h3>{vid.snippet.title}</h3>
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</figure>
+	);
 }
