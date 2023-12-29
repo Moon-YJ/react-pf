e2a64c50d4a50bf30bf4211c98f9c5c2bf3be312
Moon-YJ <tunjeong@gmail.com>
2023-12-29 11:04:01 +0900

    swiper pagination 기능 추가 및 css작업

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -3,12 +3,17 @@ import './Visual.scss';
 // npm i swiper@8
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper';
 
 export default function Visual() {
 	const { youtube } = useSelector(store => store.youtubeReducer);
 	return (
 		<figure className='Visual'>
-			<Swiper>
+			<Swiper
+				pagination={{ clickable: true }}
+				modules={[Pagination]}
+				loop={true}>
 				{youtube.map((vid, idx) => {
 					if (idx >= 5) return null;
 					return (
