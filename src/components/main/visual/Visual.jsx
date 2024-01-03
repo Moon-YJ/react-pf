cc2e827ec73634ea18e7e0bdfb211d87cd329d38
Moon-YJ <tunjeong@gmail.com>
2024-01-03 14:37:51 +0900

    swiper에서 breakpoints로 반응형 처리 설명 주석

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -54,3 +54,10 @@ export default function Visual() {
 		</figure>
 	);
 }
+
+/*
+	- Swiper의 props를 통해서 UI구조가 변경되면(slidesPerView 설정시) 해당 내용은 스크립트를 통해서 동적 제어되고 있기 때문에 일반 css로 반응형 처리 불가
+	
+	==> 따라서 breakpoints를 이용해서 브라우저 폭에 따라서 swiper의 option값 변경
+	==> 초기값으로 모바일 버전 옵션설정하고 breakpoinst로 브라우저가 늘어나는 구간마다 옵션값 변경
+*/
