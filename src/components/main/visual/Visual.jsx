2ce7348d76329d648e93120a21fa346c230e9288
Moon-YJ <tunjeong@gmail.com>
2024-01-03 12:43:44 +0900

    swiper컴포넌트 활성화이벤트(onSwiper)를 통해서 인스턴스 활용

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,48 @@
+import { useSelector } from 'react-redux';
 import './Visual.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Link } from 'react-router-dom';
+import { useRef } from 'react';
+import { Pagination, Autoplay } from 'swiper';
 
 export default function Visual() {
-	return <figure className='Visual'></figure>;
+	const Vids = useSelector(store => store.youtube.data);
+	const swiperOpt = useRef({
+		modules: [Pagination, Autoplay],
+		pagination: { clickable: true },
+		autoplay: { delay: 2000, disableOnInteraction: true },
+		loop: true,
+		slidesPerView: 3,
+		spaceBetween: 30,
+		centeredSlides: true,
+		// 맨 처음에 왼쪽 첫번째 슬라이드 비워져있는 부분 없게 처리
+		onSwiper: swiper => {
+			swiper.slideNext(300);
+		}
+	});
+	return (
+		<figure className='Visual'>
+			<Swiper {...swiperOpt.current}>
+				{Vids.map((data, idx) => {
+					if (idx >= 5) return null;
+					return (
+						<SwiperSlide key={data.id}>
+							<div className='inner'>
+								<div className='pic-box'>
+									<Link to={`/detail/${data.id}`}>
+										<img
+											src={data.snippet.thumbnails.standard ? data.snippet.thumbnails.standard.url : '/img/member1.jpg'}
+											alt={data.snippet.title}
+										/>
+									</Link>
+								</div>
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</figure>
+	);
 }
