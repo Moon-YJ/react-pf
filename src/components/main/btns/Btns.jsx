e3ce263d4c85a9d49b10a684a3ead10610326e60
Moon-YJ <tunjeong@gmail.com>
2024-01-04 11:23:59 +0900

    scroll 기본 세팅

---
+++ src/components/main/btns/Btns.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from 'react';
+import './Btns.scss';
+
+// window.scrollY: 브라우저를 스크롤할때마다 스크롤되고있는 거리값 (동적인 값)
+// DOM.scrollTop: DOM요소 안쪽에서 스크롤할때마다 스크롤되는 거리값 (동적인 값)
+// DOM.offsetTop: 문서에서 해당 돔요소의 세로 위치값 (정적인 값)
+
+export default function Btns() {
+	const [Index, setIndex] = useState(0);
+	const num = useRef(4);
+	const btns = useRef(null);
+	const sections = useRef(null);
+	const wrap = useRef(null);
+
+	useEffect(() => {
+		wrap.current = document.querySelector('.wrap');
+		sections.current = document.querySelectorAll('.myScroll');
+		wrap.current.addEventListener('scroll', e => {
+			console.log(e.target.scrollTop, '::scroll');
+			console.log(sections.current[1].offsetTop, '::offset');
+		});
+	}, []);
+
+	return (
+		<ul className='Btns'>
+			{Array(num.current)
+				.fill()
+				.map((_, idx) => {
+					return (
+						<li
+							key={idx}
+							className={idx === Index ? 'on' : ''}></li>
+					);
+				})}
+		</ul>
+	);
+}
