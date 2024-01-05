c9ef2adca2d7cf949ceeb27285a57a5ae9d77992
Moon-YJ <tunjeong@gmail.com>
2024-01-05 16:08:27 +0900

    Pics 컴포넌트에서 스크롤값 연동하여 모션 작업

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -1,5 +1,28 @@
 import './Pics.scss';
+import { useScroll } from '../../../hooks/useScroll';
+import { useEffect, useRef } from 'react';
 
 export default function Pics() {
-	return <div className='Pics myScroll'>Pics</div>;
+	const thisEl = useRef(null);
+	const boxEl = useRef(null);
+	const { scrollFrame, getCurrentScroll } = useScroll();
+
+	useEffect(() => {
+		scrollFrame?.addEventListener('scroll', () => {
+			const scroll = getCurrentScroll(thisEl.current);
+			// 닫는 순간부터 움직이게
+			if (scroll >= 0) boxEl.current.style.transform = `translateX(${scroll}px)`;
+			//boxEl.current.style.transform = `translateX(${scroll}px)`;
+		});
+	}, [scrollFrame, getCurrentScroll]);
+
+	return (
+		<section
+			className='Pics myScroll'
+			ref={thisEl}>
+			<div
+				className='box'
+				ref={boxEl}></div>
+		</section>
+	);
 }
